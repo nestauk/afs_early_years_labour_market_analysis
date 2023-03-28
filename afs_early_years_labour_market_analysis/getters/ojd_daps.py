d9dbf38e05d1cb7c0e89f0e0c10e2a81e4bb62c0
Jack-Vines <jack.vines@live.co.uk>
2023-03-28 09:08:43 +0100

    Add raw ojo extract getter

---
+++ afs_early_years_labour_market_analysis/getters/ojd_daps.py
@@ -0,0 +1,19 @@
+"""
+Getters for OJD DAPS data
+"""
+
+import pandas as pd
+
+import pandas as pd
+from nesta_ds_utils.loading_saving.S3 import download_obj
+from typing import Mapping, Union, Dict, List
+from afs_early_years_labour_market_analysis import BUCKET_NAME
+
+def get_job_adverts() -> pd.DataFrame:
+    """Returns dataframe of raw job adverts
+    """
+    return download_obj(
+        BUCKET_NAME,
+        "inputs/ojd_daps_extract/adverts_ojd_daps_extract.parquet",
+        download_as="dataframe",
+    )
